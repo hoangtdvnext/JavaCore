d95965b860f13678b1e7e3df5d595502a11c789f
hoangtd <hoangtd@vnext.vn>
2017-06-28 18:01:41 +0700

    add maven to project

--- src/JavaCNPM/contest2/C.java
+++
@@ -1,7 +0,0 @@
-package JavaCNPM.contest2;
-
-/**
- * Created by ASUS on 6/13/2017.
- */
-public class C {
-}
--- src/patterns/Behavioral/Observer/Observer.java
+++
@@ -1,9 +0,0 @@
-package patterns.Behavioral.Observer;
-
-/**
- * Created by ASUS on 5/8/2017.
- */
-public interface Observer {
-
-    void update(String message);
-}
