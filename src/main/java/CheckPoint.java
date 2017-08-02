3146af6b340e527f83678628ba1dcfd19eb39d2f
hoangtd <hoangtd@vnext.vn>
2017-08-02 17:21:28 +0700

    update code javacore

--- src/main/java/CheckPoint.java
+++ src/main/java/CheckPoint.java
@@ -1,6 +1,6 @@
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.IOException;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
 
 /**
  * Created by ASUS on 6/29/2017.
@@ -9,37 +9,50 @@ public class CheckPoint {
     private static final String FILENAME = "input.txt";
 
     public static void main(String[] args) {
-        try (BufferedReader br = new BufferedReader(new FileReader(FILENAME))) {
-            int pointA = 0, pointB = 0, pointC = 0, pointD = 0;
-            String sCurrentLine;
-            while ((sCurrentLine = br.readLine()) != null) {
-                String[] point = new String[5];
-                point = sCurrentLine.split(" ");
-                for (int i = 1; i < point.length; i++) {
-                    switch (point[i]) {
-                        case "A":
-                            pointA += i;
-                            break;
-                        case "B":
-                            pointB += i;
-                            break;
-                        case "C":
-                            pointC += i;
-                            break;
-                        case "D":
-                            pointD += i;
-                            break;
-                        default:
-                            break;
-                    }
-                }
+
+        try {
+            Scanner input = new Scanner(new File(FILENAME));
+            String s;
+            StringBuilder output = new StringBuilder();
+            for(int i=0;i<6816;i++){
+                output.append(input.nextLine()).append(",").append("\n");
             }
-            System.out.println("A :" + pointA);
-            System.out.println("B :" + pointB);
-            System.out.println("C :" + pointC);
-            System.out.println("D :" + pointD);
-        } catch (IOException e) {
+            System.out.println(output.toString());
+        } catch (FileNotFoundException e) {
             e.printStackTrace();
         }
+
+//        try (BufferedReader br = new BufferedReader(new FileReader(FILENAME))) {
+//            int pointA = 0, pointB = 0, pointC = 0, pointD = 0;
+//            String sCurrentLine;
+//            while ((sCurrentLine = br.readLine()) != null) {
+//                String[] point = new String[5];
+//                point = sCurrentLine.split(" ");
+//                for (int i = 1; i < point.length; i++) {
+//                    switch (point[i]) {
+//                        case "A":
+//                            pointA += i;
+//                            break;
+//                        case "B":
+//                            pointB += i;
+//                            break;
+//                        case "C":
+//                            pointC += i;
+//                            break;
+//                        case "D":
+//                            pointD += i;
+//                            break;
+//                        default:
+//                            break;
+//                    }
+//                }
+//            }
+//            System.out.println("A :" + pointA);
+//            System.out.println("B :" + pointB);
+//            System.out.println("C :" + pointC);
+//            System.out.println("D :" + pointD);
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        }
     }
 }
--- src/main/java/Demo.java
+++ src/main/java/Demo.java
@@ -2,8 +2,31 @@
  * Created by ASUS on 5/23/2017.
  */
 public class Demo {
+    static int x = 100;
+
+    static {
+        x += 100;
+    }
+
+    static public void method() {
+        x += 200;
+    }
 
     public static void main(String[] args) {
+        Demo o = new Demo();
+        o.x += 300;
 
+        Demo.x += 500;
+        Demo.method();
+
+        System.out.println(o.x);
+        System.out.println(Demo.x);
+
+
+        String str = "ta duy hoang";
+        if(str.contains("a")){
+            System.out.println("TRUE");
+        }
     }
+// Biến là static có giá trị như nhau, không liên quan tới việc khởi tạo object.
 }
